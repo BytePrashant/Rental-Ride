422cbfe5c1d79530e65e8874297b54bca88d6690
Prashant Tomar <prashanttomar765@gmail.com>
2024-03-14 22:31:21 +0530

    added about section

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,102 +1,11 @@
 import Link from "next/link";
+import { Navbar } from "./Navbar";
 
 export function Homepage() {
   return (
     <div className="md:mx-14">
       <img className="hidden md:flex right-0 absolute" src="./hero-bg.png" />
-      <div className="navbar flex relative top-0 right-0 left-0 p-6 justify-between items-center">
-        <div>
-          <Link href="/">
-            <img
-              src="./rental-ride-logo.png"
-              alt="logo-img"
-              className="w-[200px] h-[60px]"
-            />
-          </Link>
-        </div>
-        <ul className="navbar__links hidden md:flex gap-6 font-semibold">
-          <li>
-            <Link
-              className="home-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="about-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="models-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/"
-            >
-              Vehicle Models
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="testi-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/"
-            >
-              Testimonials
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="team-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/"
-            >
-              Our Team
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/"
-              className="contact-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <div className="navbar__buttons hidden md:flex gap-4 font-semibold items-center">
-          <Link
-            href="/login"
-            className="navbar__buttons__sign-in hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-          >
-            Sign In
-          </Link>
-          <Link
-            href="/register"
-            className="navbar__buttons__register flex justify-center text-white bg-gradient-to-br from-red-500 to-orange-400 font-semibold px-5 py-2 text-center rounded-xl w-32 hover:bg-gradient-to-br hover:from-red-600 hover:to-orange-500"
-          >
-            Register
-          </Link>
-        </div>
-        <div className="mobile-hamb md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="tabler-icon tabler-icon-menu-2"
-          >
-            <path d="M4 6l16 0"></path>
-            <path d="M4 12l16 0"></path>
-            <path d="M4 18l16 0"></path>
-          </svg>
-        </div>
-      </div>
+      <Navbar/>
       <section id="Home" className="font-custom1">
         <div className="container mx-auto px-10">
           <div className="content flex items-center justify-center h-screen w-auto relative md:justify-start">
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,76 +1,64 @@
 import { Footer } from "../components/Footer";
 import { Banner } from "../components/Banner";
 import { Plan } from "../components/Plan";
-
+import { Navbar } from "../components/Navbar";
 
 function About() {
-    return (
-      <>
-        <section className="about-page">
-          {/* <HeroPages name="About" /> */}
-          <div className="container">
-            <div className="about-main">
-              <img
-                className="about-main__img"
-                // src={AboutMain}
-                alt="car-renting"
-              />
-              <div className="about-main__text">
-                <h3>About Company</h3>
-                <h2>You start the engine and your adventure begins</h2>
-                <p>
-                  Certain but she but shyness why cottage. Guy the put instrument
-                  sir entreaties affronting. Pretended exquisite see cordially the
-                  you. Weeks quiet do vexed or whose. Motionless if no to
-                  affronting imprudence no precaution. My indulged as disposal
-                  strongly attended.
-                </p>
-                <div className="about-main__text__icons">
-                  <div className="about-main__text__icons__box">
-                    {/* <img src={Box1} alt="car-icon" /> */}
-                    <span>
-                      <h4>20</h4>
-                      <p>Car Types</p>
-                    </span>
-                  </div>
-                  <div className="about-main__text__icons__box">
-                    {/* <img src={Box2} alt="car-icon" /> */}
-                    <span>
-                      <h4>85</h4>
-                      <p>Rental Outlets</p>
-                    </span>
-                  </div>
-                  <div className="about-main__text__icons__box">
-                    {/* <img src={Box3} alt="car-icon" className="last-fk" /> */}
-                    <span>
-                      <h4>75</h4>
-                      <p>Repair Shop</p>
-                    </span>
-                  </div>
+  return (
+    <>
+      <section id="About" className="font-custom1">
+        <Navbar />
+        <div className="container mx-auto px-10">
+          <div className="about-main grid grid-rows-1 gap-8 md:flex md:flex-row">
+            <img
+              className="about-main__img"
+              src="./about-main.jpg"
+              alt="car-renting"
+            />
+            <div className="about-main__text flex flex-col text-center md:text-start md:items-start">
+              <h3 className="text-2xl font-semibold">About Company</h3>
+              <h2 className="text-4xl font-bold my-6 md:leading-tight md:mt-2">
+                You start the engine and your adventure begins
+              </h2>
+              <p className="text-base text-gray-600 mb-10">
+                Certain but she but shyness why cottage. Guy the put instrument
+                sir entreaties affronting. Pretended exquisite see cordially the
+                you. Weeks quiet do vexed or whose. Motionless if no to
+                affronting imprudence no precaution. My indulged as disposal
+                strongly attended.
+              </p>
+              <div className="about-main__text__icons flex flex-col gap-6">
+                <div className="about-main__text__icons__box flex flex-col items-center gap-2">
+                  <img src="./about-icon1.png" alt="car-icon" />
+                  <span>
+                    <h4 className="text-4xl font-bold">20</h4>
+                    <p className="text-base text-gray-600 mb-6">Car Types</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box flex flex-col items-center gap-2">
+                  <img src="./about-icon2.png" alt="car-icon" />
+                  <span>
+                    <h4 className="text-4xl font-bold">85</h4>
+                    <p className="text-base text-gray-600 mb-6">Rental Outlets</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box flex flex-col items-center gap-2">
+                  <img src="./about-icon3.png" alt="car-icon" className="last-fk" />
+                  <span>
+                    <h4 className="text-4xl font-bold">75</h4>
+                    <p className="text-base text-gray-600 mb-6">Repair Shop</p>
+                  </span>
                 </div>
               </div>
-            </div>
-            {/* <PlanTrip /> */}
-          </div>
-        </section>
-        <div className="book-banner">
-          <div className="book-banner__overlay"></div>
-          <div className="container">
-            <div className="text-content">
-              <h2>Book a car by getting in touch with us</h2>
-              <span>
-                {/* <IconPhone width={40} height={40} /> */}
-                <h3>(123) 456-7869</h3>
-              </span>
             </div>
           </div>
         </div>
         <Plan />
         <Banner />
         <Footer />
-      </>
-    );
-  }
-  
-  export default About;
-  
+      </section>
+    </>
+  );
+}
+
+export default About;
