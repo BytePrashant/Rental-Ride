770997453a08cecaea78d2c98f4859c0dd7a70bf
Prashant Tomar <prashanttomar765@gmail.com>
2024-03-27 23:35:51 +0530

    Added link in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,7 +33,7 @@ export function Navbar() {
           <li>
             <Link
               className="models-link hover:text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-400"
-              href="/"
+              href="/vehicles"
             >
               Vehicle Models
             </Link>
