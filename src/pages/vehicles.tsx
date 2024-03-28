8070921b045cc64ca1f5f5057f232eaeea6ff2e4
Prashant Tomar <prashanttomar765@gmail.com>
2024-03-29 00:41:30 +0530

    component aligned

--- src/pages/vehicles.tsx
+++ src/pages/vehicles.tsx
@@ -1,17 +1,28 @@
-import { Banner } from "../components/Banner"
-import { Footer } from "../components/Footer"
-import { Navbar } from "../components/Navbar"
-import {VehicleModels} from "../components/VehicleModels"
+import { Banner } from "../components/Banner";
+import { Footer } from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import { VehicleModels } from "../components/VehicleModels";
 
 const Vehicle = () => {
-    return (
-        <>
-            <Navbar/>
-            <VehicleModels/>
-            <Banner/>
-            <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <section id="Vehicle-Models" className="font-custom1 py-16 md:mx-14">
+        <div className="container mx-auto px-10 md:flex">
+          <div className=" grid grid-rows-2 grid-cols-3 gap-4">
+            <VehicleModels />
+            <VehicleModels />
+            <VehicleModels />
+            <VehicleModels />
+            <VehicleModels />
+            <VehicleModels />
+          </div>
+        </div>
+      </section>
+      <Banner />
+      <Footer />
+    </>
+  );
+};
 
 export default Vehicle;
